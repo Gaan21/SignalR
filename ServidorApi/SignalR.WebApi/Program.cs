bd6f615a6d5933e835c6b09e77066e418de1cfb6
Gaan21 <lucianoplazagrueso21@gmail.com>
2023-05-27 19:03:43 +0200

    Añadida config de Cors para recibir solicitudes de Angular

--- ServidorApi/SignalR.WebApi/Program.cs
+++ ServidorApi/SignalR.WebApi/Program.cs
@@ -1,4 +1,7 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
+string CorsConfiguration = "_corsConfiguration";
 
 // Add services to the container.
 
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CorsConfiguration,
+            builder =>
+            {
+                builder.WithOrigins("http://localhost:4200");
+            });
+});
 
 var app = builder.Build();
 
@@ -19,6 +32,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(CorsConfiguration);
 
 app.MapControllers();
 
